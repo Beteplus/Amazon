5c83d9a94308a72006f3af635cc94a3e484858c4
bete <beteme390@gmail.com>
2024-04-10 05:15:12 -1100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Initialize Swiper
+//     var Swiper = new Swiper(".mySwiper", {
+//       slidesPerView: 1,
+//       spaceBetween: 0,
+//       loop: true,
+//       autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//       },
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//     });
+//   });
